13d4310de54e877c0c3ee25d97623aeb3c579999
Allen Garvey <allen.garvey@lightspeedhq.com>
2024-08-20 21:08:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Video Seek Example
+
+An example project for loading a video and rendering a frame to a canvas, with an adjustable seek time.
+
+## Dependencies
+
+* Modern web browser
+
+## Getting Started
+
+* Start a local webserver from the `public` directory
+* 
+
+## License
+
+Video Seek Example is released under the MIT License. See license.txt for more details.
---
+++ public/assets/app.js
@@ -0,0 +1,51 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext("2d");
+const video = document.getElementById('video');
+const fileInput = document.getElementById('file_picker');
+const seeker = document.getElementById('seek');
+let videoLoaded = false;
+
+const drawFrame = () => {
+    context.drawImage(video, 0, 0);
+};
+
+video.addEventListener("loadedmetadata", () => {
+    seeker.disabled = false;
+    seeker.value = 0;
+    seeker.max = video.duration;
+});
+
+video.addEventListener("canplaythrough", () => {
+    if (videoLoaded) {
+        return;
+      }
+    console.log('data loaded');
+    videoLoaded = true;
+    canvas.height = video.videoHeight;
+    canvas.width = video.videoWidth;
+
+    setTimeout(() => {
+        drawFrame();
+    }, 1000);
+});
+
+
+seeker.addEventListener("change", () => {
+    if (!videoLoaded) {
+      return;
+    }
+    video.currentTime = seeker.value;
+    drawFrame();
+});
+
+
+fileInput.addEventListener("change", () => {
+    if (fileInput.files.length < 1) {
+      return;
+    }
+    videoLoaded = false;
+    const file = fileInput.files[0];
+
+    video.src = URL.createObjectURL(file);
+    video.load();
+});
